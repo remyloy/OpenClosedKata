88a5f5743540ddfaa9dd60668adccc1cdfc6c9a6
Remy Loy <remy.loy@generic.de>
2017-06-23 14:37:44 +0200

    Kunde2 SendMail implemented

---
+++ BewerberAuswahl.Kunde2/Program.fs
@@ -0,0 +1,21 @@
+﻿open BewerberAuswahl
+open System
+
+let sendEmail candidates =
+    candidates
+    |> List.filter (fun c -> c.Validation |> function | Valid -> true | Error _ -> false)
+    |> List.map (fun c -> c.Value)
+    |> List.choose (fun c -> c.Email |> Option.map (fun email -> c.Name, email))
+    |> List.iter (fun (name, (Email email)) ->
+        printfn "send.exe to=%s body='Sehr geehrter %s, wir möchten Sie einladen.'" email name)
+
+[<EntryPoint>]
+let main argv = 
+    let filters =
+        Logic.combine [Logic.filterHasUmlaut; Logic.filterTooOld (Age 25); Logic.filterByNames (["Carsten"] |> Set.ofList)]
+
+    Logic.parseCSV ';' "BewerberListe.csv"
+    |> Logic.validate filters
+    |> sendEmail
+    
+    0 // return an integer exit code
